4fbc74e443639a8a8b473af5fcf65c2149582990
omaralshehri <Alshehri2292@gmail.com>
2024-02-11 21:33:42 +0300

    add sort(array)

--- src/Sorter.java
+++ src/Sorter.java
@@ -15,26 +15,29 @@ public class Sorter {
         }
     }
     public int[] selectionSort(int[] list) {
-        int i, j, minValue, minIndex, temp = 0;
-        for (i = 1; i < list.length; i++) {
+        // Remove the redundant declaration of 'j' here
+        int i, minValue, minIndex, temp = 0;
+        for (i = 0; i < list.length - 1; i++) {
             minValue = list[i];
             minIndex = i;
-            j = i - 1;
-            for (j = i; j < list.length; j++) {
-
+            // Remove the redundant declaration of 'j' here
+            for (int j = i + 1; j < list.length; j++) {
                 if (list[j] < minValue) {
                     minValue = list[j];
                     minIndex = j;
                 }
-
             }
-            if (list[i] > minValue) {
+            if (minValue < list[i]) {
                 temp = list[i];
                 list[i] = list[minIndex];
                 list[minIndex] = temp;
             }
-
         }
+
         return list;
+    }
+    public int[] sort(int[] array) {
+        // Call the selectionSort method to sort the array
+        return selectionSort(array);
     }
 }
